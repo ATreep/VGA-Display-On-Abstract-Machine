cb8cc6dfcfc254543828bd03816170fae28bc368
Treep <101784431+ATreep@users.noreply.github.com>
2025-01-30 19:58:47 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <klib.h>
 
 int main() {
-    print_vga("Hello, World! This text is showing on VCG window.");
+    print_vga("Hello, World! This text is showing on VGA window.");
     print("Hello, World! This text is showing on serial console.\n");
     while(1);
     return 0;
